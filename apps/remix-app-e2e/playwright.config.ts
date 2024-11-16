b6a55c1a6e130650eb2df1824bf4011c35c55a25
Stéphane Le Dorze <stephane.ledorze@gmail.com>
2024-11-16 13:12:04 +0000

    e2e tests are working

--- apps/remix-app-e2e/playwright.config.ts
+++ apps/remix-app-e2e/playwright.config.ts
@@ -26,25 +26,31 @@ export default defineConfig({
   /* Run your local dev server before starting the tests */
   webServer: {
     command: 'npx nx run remix-app:serve-static',
-    url: 'http://localhost:3000',
+    url: 'http://0.0.0.0:3000',
     reuseExistingServer: !process.env.CI,
     cwd: workspaceRoot,
   },
+  reporter: [
+    ['html', { 
+      host: '0.0.0.0',
+      port: 9323
+    }]
+  ],
   projects: [
     {
       name: 'chromium',
       use: { ...devices['Desktop Chrome'] },
     },
 
-    {
-      name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
-    },
+    // {
+    //   name: 'firefox',
+    //   use: { ...devices['Desktop Firefox'] },
+    // },
 
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] },
+    // },
 
     // Uncomment for mobile browsers support
     /* {
